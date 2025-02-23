1aee6170d903d981c2b5707ca469dee48f4625d5
Prathamesh Kapadne <prathamesh.kapadne@mitaoe.ac.in>
2025-02-23 19:03:27 +0530

    some changes done

--- main2.py
+++ main2.py
@@ -40,5 +40,6 @@ def predict():
 
     return jsonify({"prediction": int(prediction[0]), "probability": float(probability)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
